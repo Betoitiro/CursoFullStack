80411b4817770defc1ac164c979b35d13d97b488
Betoitiro <itirotrabalhos@gmail.com>
2024-12-15 15:07:29 -0300

    ,

---
+++ curso-fullStack/bookspot/src/main/java/com/book/spot/bookspot/BookspotApplication.java
@@ -0,0 +1,13 @@
+package com.book.spot.bookspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookspotApplication.class, args);
+	}
+
+}
---
+++ curso-fullStack/bookspot/src/test/java/com/book/spot/bookspot/BookspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.spot.bookspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
