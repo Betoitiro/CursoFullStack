3ce11b798b6d0e74aa63bb1333a364e1f19ec105
Betoitiro <itirotrabalhos@gmail.com>
2024-12-15 15:04:43 -0300

    erro no vizualzar resenha

--- curso-fullStack/book-spot/src/pages/VisualizarResenha.jsx
+++ curso-fullStack/book-spot/src/pages/VisualizarResenha.jsx
@@ -2,26 +2,30 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 
-const VisualizarResenha = () => {
-    const { id } = useParams()
-    conse [resenha, setResenha] = useState()
+const VisualizaResenha = () => {
 
-    /**
-     *useEffect para pegar uma resenha especifica. [id] -> so carrega quando tiver o id
-     */
-    useEffect(()=> {
+    const { id } = useParams()
+    const [resenha, setResenha] = useState({})
+
+    /*
+    useEffect para fazer o get na resenha
+    especifica. [id] -> só carrega quando tiver
+    o id
+    */
+
+    useEffect(() => {
         axios.get(`http://localhost:3000/resenhas/${id}`)
-        console.log(URL)
         .then(response => setResenha(response.data))
-        .catch(error => console.error(`rro ao consultar as resenhas de id ${id}`,error))
+        .catch(error => console.error("Erro ao carregar a resenha ", error))
     }, [id])
+
   return (
     <div>
         <h2>{resenha.tituloLivro}</h2>
-        <h3>Por: {resenha.autor}</h3>
+        <h3>Por {resenha.autor}</h3>
         <p>{resenha.conteudoCompleto}</p>
     </div>
   )
 }
 
-export default VisualizarResenha
+export default VisualizaResenha
